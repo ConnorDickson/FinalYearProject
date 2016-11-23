4e78c0427ebf5dfbfac4f54d9e4bc2a4fd6da785
Connor Dickson <cdickson14@qub.ac.uk>
2016-11-23 01:37:51 +0000

    Connor: Updated files after trying to get docker to communicate with redis.

--- EdgeNode/DockerCachingApplication/src/server.js
+++ EdgeNode/DockerCachingApplication/src/server.js
@@ -1,16 +1,48 @@
 var util = require('util');
 var os = require('os');
-var hostname = os.hostname();
+var redis = require('redis');
 
 util.debug("Starting...");
 
+var hostname = os.hostname();
 var http = require('http');
 var port = process.env.port || 3001;
+
 http.createServer(function (req, res) {
-    util.debug("Get request");
+    console.error("Get request");
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end("Caching App\nContainer:"+  hostname);
-    util.debug("Send response");
+    console.error("Send response");
 }).listen(port);
  
-util.debug("Started");
+console.error("Started Node.js server");
+
+var redishost = 'edgenode01';
+var redisport = '6379';
+
+var redisclient = redis.createClient(redisport,redishost);
+
+/*function TryConnectToRedis() 
+{
+    try
+    {
+        redisclient = redis.createClient(6379, edge01);
+        return true;
+    }
+    catch(ex)
+    {
+        return false;
+    }
+
+    return false;
+}
+
+while(!TryConnectToRedis()) {
+    setTimeout(function() {
+        console.error("Could not connect to redis, trying again after 10 seconds");
+    }, 10000);
+}*/
+
+redisclient.on('connect',function () {
+   console.error("Connected to redis");
+});
