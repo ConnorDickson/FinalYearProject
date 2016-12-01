599efde9af087fdf1d6db6eb456a26a4b2751e85
Connor Dickson <cdickson14@qub.ac.uk>
2016-12-01 01:15:24 +0000

    Connor: Got a working version of the Caching Application for Client Device - Edge Node - Data Centre

--- Client/ElectronApp/JS/CachingApplication.js
+++ Client/ElectronApp/JS/CachingApplication.js
@@ -1,3 +1,27 @@
+onload = () => 
+{
+    var sendTime = 1.0;
+    var receiveTime = 1.0;
+    
+    const webview = document.getElementById("WebView");
+    
+    const loadstart = () => 
+    {
+        document.getElementById("requestTime").innerHTML = "Started Request";
+        sendTime = (new Date()).getTime();
+    }
+    
+    const loadstop = () => 
+    {
+        receiveTime = (new Date()).getTime();
+        
+        document.getElementById("requestTime").innerHTML = "Total Request Time: " + ((receiveTime - sendTime)/1000).toFixed(2) + " seconds";
+    }
+    
+    webview.addEventListener('did-start-loading', loadstart);
+    webview.addEventListener('did-stop-loading', loadstop);
+}
+
 function NavigateHome() 
 {
     window.location.href = "../index.html";
@@ -5,7 +29,7 @@ function NavigateHome()
 
 function NavigateBrowser() 
 {
-    var url = document.getElementById("urlAddress").value;
+    var url = "http://edgepi01:3000/?url=" + document.getElementById("urlAddress").value;
     var browser = document.getElementById("WebView");
     browser.setAttribute("src", url);
 }
--- EdgeNode/DockerCachingApplication/src/server.js
+++ EdgeNode/DockerCachingApplication/src/server.js
@@ -12,32 +12,57 @@ var redisport = '6379';
 
 console.log("Starting...");
 
-http.createServer(function (req, res) 
+var createdServer = http.createServer(function (req, res) 
 {
-    console.log("Get request for: " + req.url);
-    
-    var url_parts = url.parse(req.url,true);
-    var query = url_parts.query;
-    var requestedUrl = query.url;
+    //Set error handlers
+    req.on('error', function(err) 
+    {
+        console.error("REQUEST ERROR:\n" + err.stack);
+    });
 
-    console.log("Going to make custom request to " + requestedUrl);
+    res.on('error', function(err)
+    {
+        console.error("RESPONSE ERROR:\n" + err.stack);
+    });
 
     if(req.url == "/clearcache")
     {
         //clear redis cache here
-        
+        console.log("Clear Redis cache");   
     }
+
+    //Get Query String request
+    var url_parts = url.parse(req.url,true);
+    var query = url_parts.query;
+    var requestedUrl = query.url;
 
     if(typeof requestedUrl == 'undefined') 
     {
-        console.log("Recieved undefined request");
-        res.writeHead(200, {'Content-Type': 'image/x-icon'});
-        res.end();
-        return;    
-    }
+        //This is a follow on request to the caching service
+        console.log("Get request for: " + req.url + " but it had an undefined querystring");
+        
+        var referer = req.headers.referer;
+        var refererUrlParts = url.parse(referer,true);
+        var refererQuery = refererUrlParts.query;
+        var refererRequestedUrl = refererQuery.url; 
+        //GetOrSetRequestValueFromRedis("http://google.com" + req.url,res);
+        GetOrSetRequestValueFromRedis(refererRequestedUrl + req.url,res);
+    } else {
+        //This is a standard request to the caching service
+        GetOrSetRequestValueFromRedis(requestedUrl,res);
+    }    
+});
 
-    //Check redis first to see if we already have the value
-    //Check if this method is async
+//I don't think I should do this in production because the code continues
+createdServer.on('error',function(err)
+{
+    console.error("An error occurred with the server: " + err.stack);
+});
+
+createdServer.listen(port);
+
+function GetOrSetRequestValueFromRedis(requestedUrl, res) 
+{
     redisclient.get(requestedUrl,function(err,reply)
     {
         if(err) 
@@ -48,26 +73,67 @@ http.createServer(function (req, res)
         {
             if(reply != null) 
             {
-                console.log("Found value in redis");
+                console.log("Found " + requestedUrl + " value in redis");
                 redisResponse = reply.toString();
+                res.writeHead(200, {'Content-Type':'text/html'});
                 res.end(redisResponse);
                 return;
+            } else {
+                MakeAndStoreRequest(requestedUrl,res);
             }
         }
     });
+}
 
-    //COMMENT THIS
-    request(requestedUrl, function(error,response,body) 
+function MakeAndStoreRequest(requestedUrl, res) 
+{
+    console.log("Going to make custom request to " + requestedUrl);
+
+    /*if(requestedUrl.includes(".gif")) 
     {
-        res.end(body);
-        console.log("Completed request and going to store in Redis");
-        redisclient.set(requestedUrl,body);
-        redisclient.expire(requestedUrl,60);
-    });
+        console.log("Got gif request");
 
-    console.log("Sent response");
+        http.get(requestedUrl, function(imageRes)
+        {
+            if(typeof imageRes == 'undefined')
+            {
+                console.log("Image request was undefined");
+                res.end();
+            } else {
+                res.writeHead(200, {'Content-Type':'image/gif'});
+                res.end(imageRes,'binary');
+            }
+        });
+    } else {*/
+        request(requestedUrl, function(error,response,body) 
+        {
+            //console.log("Custom Request results\n\tError: " + error + ".\n\tResponse: " + response + ".\n\tBody: " + body + ".");
 
-}).listen(port);
+            if(error)
+            {
+                console.error("There was an error with the custom request: " + error.stack);
+            }
+	    else if(typeof body == 'undefined') 
+	    {
+                //DO IMAGE STUFF HERE BECAUSE THIS MUST BE BINARY?
+		//Does this mean I need to store content type in redis?
+	        console.log("Not storing " + requestedUrl + " in redis as body is undefined");
+	        res.end();
+	    } 
+            else 
+            {
+                //This was a successful request
+                res.writeHead(200, {'Content-Type':'text/html'});
+	        res.end(body);
+	        console.log("Completed request and going to store " + requestedUrl + " in Redis");
+	        redisclient.set(requestedUrl,body);
+	        redisclient.expire(requestedUrl,60);
+	    }
+        }).on('error',function(err) {
+            console.error("ERROR WITH CUSTOM REQUEST: " + err.stack);
+        });
+    /*}*/
+}
  
 console.log("Started Node.js server");
 
@@ -75,30 +141,12 @@ console.log("Trying to connect to redis");
 
 var redisclient = redis.createClient(6379,'edgepi01',{no_ready_check: true});
 
-/*redisclient.auth('password',function(err) 
-{
-   if(err)
-   {
-      console.error(err);
-   } 
-});*/
-
 redisclient.on('connect',function () 
 {
    console.log("Connected to Redis");
 });
 
-/*redisclient.set("foo","bar",redis.print);
-
-redisclient.get("foo",function(err,reply)
+redisclient.on('error',function(err) 
 {
-    if(err) 
-    {
-        console.error(err);
-    }
-    else
-    {
-        redisResponse = reply.toString();
-        console.log(reply.toString());
-    }
-});*/
+    console.error("REDIS ERROR:\n" + err.stack);
+});
