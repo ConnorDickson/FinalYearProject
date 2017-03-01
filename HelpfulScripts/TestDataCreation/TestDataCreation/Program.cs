fb724f07d423e0fb950843189a890d7713b83c5c
Connor Dickson <cdickson14@qub.ac.uk>
2017-03-01 09:08:00 +0000

    Connor: Updated Dissertation.docx and added References.txt for websites I used for different applications. I also created the basis for a test data creation solution

---
+++ HelpfulScripts/TestDataCreation/TestDataCreation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace TestDataCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfLines = 0;
+
+            if (args.Length > 0)
+            {
+                int.TryParse(args[0], out numberOfLines);
+            }
+
+            if(numberOfLines == 0)
+            {
+                numberOfLines = 1000;
+            }
+
+            CreateData(numberOfLines);
+        }
+
+        private static void CreateData(int numberOfLines)
+        {
+            File.Create(Directory.GetCurrentDirectory().Trim('/') + "/TestData.txt");
+            //need 4 true and falses in this format "True,True,False,False" with a line break
+            
+            for(int i = 0; i < numberOfLines; i++)
+            {
+                var rand = new Random();
+                var result = rand.Next(2) <= 1 ? true : false;
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
