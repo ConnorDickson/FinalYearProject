ad85f3733d0dc1f2d4f1fbd2e5d97cd262afd731
Connor Dickson <cdickson14@qub.ac.uk>
2017-04-09 12:27:39 +0100

    Connor: Added automation to load balance tests. Setup load balance spreadsheet and experiment section in Dissertation.docx

--- Client/ElectronApp/JS/LoadBalancing.js
+++ Client/ElectronApp/JS/LoadBalancing.js
@@ -1,18 +1,88 @@
 //This already has the const declared because I am extending the voice recognition file
-var localStopwatch;
+var startTime;
 var requestsLeft;
 var initialCountOfRequests;
+
+//Variables for requests
+var requestTimeRecords = [];
 var localCPURecords = [];
 var remoteCPURecords = [];
+var dataCentreCPURecords = [];
+var localRAMRecords = [];
+var edgeRAMRecords = [];
+var dataCentreRAMRecords = [];
 
-//Creates the stopwatch object
-window.onload = function() {
-    var localStopwatchElement = document.getElementById('localStopwatchResults');
-    localStopwatch = new Stopwatch(localStopwatchElement);    
-};
+//Variables for experimentation
+var totalAverageTime = [];
+var totalAverageRequestTime = [];
+var totalAverageLocalCPU = [];
+var totalAverageEdgeCPU = [];
+var totalAverageDataCentreCPU = [];
+var totalAverageLocalRAM = [];
+var totalAverageEdgeRAM = [];
+var totalAverageDataCentreRAM = [];
+
+var remoteExecutionCounter = 0;
+var remoteExecuionsRequired = 10;
+
+function PerformExperiment() {
+    remoteExecutionCounter = 0;
+    
+    totalAverageTime = [];
+    totalAverageRequestTime = [];
+    totalAverageLocalCPU = [];
+    totalAverageEdgeCPU = [];
+    totalAverageDataCentreCPU = [];
+    totalAverageLocalRAM = [];
+    totalAverageEdgeRAM = [];
+    totalAverageDataCentreRAM = [];
+        
+    var timeBetweenRequests = 30000;
+    
+    //Clear cache after X time and print to UI saying that's what is going to happen
+    for(var requestNumber = 0; requestNumber < remoteExecuionsRequired; requestNumber++) {        
+        setTimeout(ExecuteVoiceRecognitionLoadBalance.bind(this, recordExperiment), (timeBetweenRequests * requestNumber));
+    }
+}
+
+function recordExperiment() {
+    remoteExecutionCounter++;
+    
+    if(remoteExecutionCounter == remoteExecuionsRequired) {
+        document.getElementById('localStopwatchResults').innerHTML = "Time: ";
+        document.getElementById('averageRequestTimeResult').innerHTML = "Average request time: ";
+        document.getElementById('averageLocalCPUResult').innerHTML = "Average local CPU: ";
+        document.getElementById('averageRemoteCPUResult').innerHTML = "Average edge CPU: ";
+        document.getElementById('averageDataCentreCPUResult').innerHTML = "Average data centre CPU: ";
+        document.getElementById('averageLocalRAMResult').innerHTML = "Average local RAM: ";
+        document.getElementById('averageRemoteRAMResult').innerHTML = "Average edge RAM: ";
+        document.getElementById('averageDataCentreRAMResult').innerHTML = "Average data centre RAM: ";
+
+        for(var i = 0; i < totalAverageTime.length; i++) {
+            document.getElementById('localStopwatchResults').innerHTML = document.getElementById('localStopwatchResults').innerHTML + totalAverageTime[i] + " ";
+            document.getElementById('averageRequestTimeResult').innerHTML = document.getElementById('averageRequestTimeResult').innerHTML + totalAverageRequestTime[i].toFixed(3) + " ";
+            document.getElementById('averageLocalCPUResult').innerHTML = document.getElementById('averageLocalCPUResult').innerHTML + totalAverageLocalCPU[i].toFixed(2) + " ";
+            document.getElementById('averageRemoteCPUResult').innerHTML = document.getElementById('averageRemoteCPUResult').innerHTML + totalAverageEdgeCPU[i].toFixed(2) + " ";
+            document.getElementById('averageDataCentreCPUResult').innerHTML = document.getElementById('averageDataCentreCPUResult').innerHTML + totalAverageDataCentreCPU[i].toFixed(2) + " ";
+            document.getElementById('averageLocalRAMResult').innerHTML = document.getElementById('averageLocalRAMResult').innerHTML + totalAverageLocalRAM[i].toFixed(2) + " ";
+            document.getElementById('averageRemoteRAMResult').innerHTML = document.getElementById('averageRemoteRAMResult').innerHTML + totalAverageEdgeRAM[i].toFixed(2) + " ";
+            document.getElementById('averageDataCentreRAMResult').innerHTML = document.getElementById('averageDataCentreRAMResult').innerHTML + totalAverageDataCentreRAM[i].toFixed(2) + " ";
+        }
+
+        document.getElementById('localStopwatchResults').innerHTML = document.getElementById('localStopwatchResults').innerHTML + " seconds.";   
+    }
+}
 
 //called from button to execute the load balance request
-function ExecuteVoiceRecognitionLoadBalance() {
+function ExecuteVoiceRecognitionLoadBalance(callback) {
+    requestTimeRecords = [];
+    localCPURecords = [];
+    remoteCPURecords = [];
+    dataCentreCPURecords = [];
+    localRAMRecords = [];
+    edgeRAMRecords = [];
+    dataCentreRAMRecords = [];
+    
     var numberOfRequests = parseInt(document.getElementById('NumberOfVoiceRecognitionRequests').value);
 
     if(numberOfRequests > 100) {
@@ -20,13 +90,18 @@ function ExecuteVoiceRecognitionLoadBalance() {
         return;
     }
     
+    document.getElementById('localStopwatchResults').innerHTML = "Processing...";
+    document.getElementById('averageRequestTimeResult').innerHTML = "Processing...";
     document.getElementById('averageLocalCPUResult').innerHTML = "Processing...";
     document.getElementById('averageRemoteCPUResult').innerHTML = "Processing...";
+    document.getElementById('averageDataCentreCPUResult').innerHTML = "Processing...";
+    document.getElementById('averageLocalRAMResult').innerHTML = "Processing...";
+    document.getElementById('averageRemoteRAMResult').innerHTML = "Processing...";
+    document.getElementById('averageDataCentreRAMResult').innerHTML = "Processing...";
     document.getElementById('requestsLeft').innerHTML = "Processing...";
     
-    localStopwatch.reset();
-    localStopwatch.start();
-    
+    startTime = Date.now();
+
     document.getElementById('VoiceRecognitionResults').innerHTML = "";
     
     cpu.cpuStart();
@@ -37,14 +112,16 @@ function ExecuteVoiceRecognitionLoadBalance() {
     //used to spawn multiple concurrent requests
     if(numberOfRequests > 0) {
         while(numberOfRequests > 0) {
-            setTimeout(ExecuteLoadBalanceRemoteVoiceRecognition, 0);            
+            setTimeout(ExecuteLoadBalanceRemoteVoiceRecognition.bind(this, callback, 0));            
             numberOfRequests--;
         }
     }
 }
 
 //executes a single request but called multiple times concurrently
-function ExecuteLoadBalanceRemoteVoiceRecognition() {
+function ExecuteLoadBalanceRemoteVoiceRecognition(callback) {        
+    var requestStartTime = Date.now();
+    
     //read the file async so that other threads are not held up
     fs.readFile("../../Downloads/output.wav", (err, data) => {
         var urlToPostTo = 'http://connor-pc:3000/api/voicerecognition/PostVoiceRequest';
@@ -84,13 +161,41 @@ function ExecuteLoadBalanceRemoteVoiceRecognition() {
                 document.getElementById('requestsLeft').innerHTML = "Requests left: " + requestsLeft + '/' + initialCountOfRequests;                
                 
                 if(requestsLeft == 0) {
-                    localStopwatch.stop();
+                    var endTime = Date.now();
+                    var totalTime = (endTime - startTime)/1000;
+                    document.getElementById('localStopwatchResults').innerHTML = "Time: " + totalTime + " seconds.";
+                    
+                    var averageRequestTimeResult = Average(requestTimeRecords);
                     var averageLocalCPUResult = Average(localCPURecords);
                     var averageRemoteCPUResult = Average(remoteCPURecords);
-                    document.getElementById('averageLocalCPUResult').innerHTML = "Average local CPU: " + averageLocalCPUResult;
-                    document.getElementById('averageRemoteCPUResult').innerHTML = "Average remote CPU: " + averageRemoteCPUResult;
+                    var averageDataCentreCPUResult = Average(dataCentreCPURecords);
+                    var averageLocalRAMResult = Average(localRAMRecords);
+                    var averageEdgeRAMResult = Average(edgeRAMRecords);
+                    var averageDataCentreRAMResult = Average(dataCentreRAMRecords);
+                                        
+                    totalAverageTime.push(totalTime);
+                    totalAverageRequestTime.push(averageRequestTimeResult);
+                    totalAverageLocalCPU.push(averageLocalCPUResult);
+                    totalAverageEdgeCPU.push(averageRemoteCPUResult);
+                    totalAverageDataCentreCPU.push(averageDataCentreCPUResult);
+                    totalAverageLocalRAM.push(averageLocalRAMResult);
+                    totalAverageEdgeRAM.push(averageEdgeRAMResult);
+                    totalAverageDataCentreRAM.push(averageDataCentreRAMResult);
+                    
+                    document.getElementById('averageRequestTimeResult').innerHTML = "Average request time: " + averageRequestTimeResult.toFixed(2);
+                    document.getElementById('averageLocalCPUResult').innerHTML = "Average local CPU: " + averageLocalCPUResult.toFixed(2);
+                    document.getElementById('averageRemoteCPUResult').innerHTML = "Average edge CPU: " + averageRemoteCPUResult.toFixed(2);
+                    document.getElementById('averageDataCentreCPUResult').innerHTML = "Average data centre CPU: " + averageDataCentreCPUResult.toFixed(2);
+                    document.getElementById('averageLocalRAMResult').innerHTML = "Average local RAM: " + averageLocalRAMResult.toFixed(2);
+                    document.getElementById('averageRemoteRAMResult').innerHTML = "Average edge RAM: " + averageEdgeRAMResult.toFixed(2);
+                    document.getElementById('averageDataCentreRAMResult').innerHTML = "Average data centre RAM: " + averageDataCentreRAMResult.toFixed(2);
+                    
+                    if(callback) {
+                        callback();
+                    }
                 }
-                SetVoiceRecognitionResults(responseData);
+                
+                SetVoiceRecognitionResults(responseData, requestStartTime);
             });
         }); 
     });
@@ -108,11 +213,19 @@ function Average(elements) {
 }
 
 //obtain and set the voice recognition results
-function SetVoiceRecognitionResults(responseData) {
+function SetVoiceRecognitionResults(responseData, requestStartTime) {
     var load = cpu.cpuEnd();
     var freeMemory = cpu.freeMemory();
     var jsonResult = JSON.parse(responseData);
+    var requestEndTime = Date.now();
+    var totalTime = (requestEndTime - requestStartTime)/1000;
+    var dataCentreCPU = parseFloat(jsonResult.DataCentreProcessor).toFixed(2)
+    requestTimeRecords.push(totalTime);
     localCPURecords.push(load.percent);
     remoteCPURecords.push(jsonResult.CPUInfo);
-    document.getElementById('VoiceRecognitionResults').innerHTML += "<br>Results: " + jsonResult.VoiceRecognitionResponse.trim() + " Local: " + load.percent + " Remote: " + jsonResult.CPUInfo;
+    dataCentreCPURecords.push(dataCentreCPU);
+    localRAMRecords.push(freeMemory);
+    edgeRAMRecords.push(jsonResult.RAMInfo);
+    dataCentreRAMRecords.push((jsonResult.DataCentreMemory/1000000).toFixed(2));
+    document.getElementById('VoiceRecognitionResults').innerHTML += "<br>Results: " + jsonResult.VoiceRecognitionResponse.trim() + " Local CPU: " + load.percent + " Edge CPU: " + jsonResult.CPUInfo + " Data Centre CPU: " + dataCentreCPU + " Total Time: " + totalTime;
 }
